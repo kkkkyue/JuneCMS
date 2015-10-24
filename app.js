1cf09595649be4c8710496d6ffa4b6ab9cd46bfc
王俊 <junewong@outlook.com>
2015-10-24 18:40:03 +0800

    添加模板

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 ﻿var koa = require('koa');
+var render = require('koa-ejs');
 var router = require('./Modules/Router.js');
 var koaBody = require('koa-body')();
 var app = koa();
 
+//模板设置
+var filters = {
+    format: function (time) {
+        return time.getFullYear() + '-' + (time.getMonth() + 1) + '-' + time.getDate();
+    }
+};
+
+render(app, {
+    root: path.join(__dirname, 'views'),
+    layout: 'template',
+    viewExt: 'html',
+    cache: false,
+    debug: true,
+    filters: filters
+});
+
 //应用路由配置
 router.routes(app);
-//app.use(function*() {
-//    this.body = 'Hello World';
-//});
+
 
 
 
