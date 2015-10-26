bff050b8ad6125ba18476aaa172d824c06e72150
kkkkyue <junewong@outlook.com>
2015-10-26 18:34:06 +0800

    fix 0.05

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,17 +1,4 @@
-//(function($) {
-//  'use strict';
 
-//  $(function() {
-//    var $fullText = $('.admin-fullText');
-//    $('#admin-fullscreen').on('click', function() {
-//      $.AMUI.fullscreen.toggle();
-//    });
-
-//    $(document).on($.AMUI.fullscreen.raw.fullscreenchange, function() {
-//      $fullText.text($.AMUI.fullscreen.isFullscreen ? '退出全屏' : '开启全屏');
-//    });
-//  });
-//})(jQuery);
 define(function(require, exports, module) {
 
     // 通过 require 引入依赖
